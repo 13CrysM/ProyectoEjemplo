e497db4d3b13786087ad210fad2a1bc8d1adfe72
13CrysM <crysmupe@gamil.com>
2024-10-10 23:14:34 -0600

    Comentarios en codigo

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 int main (void)
 {
     printf("Hola Mundo");
+    //git clone: clonaba (copia) del repositorio
+    //git fetch: descargaba la informacion
+    //git pull origin main: 
+    //git commit -am "mensaje": guardar local los elementos 
+    //git push origin main: empuja los cambios al repositorio
+    //git
+    //git
 }
